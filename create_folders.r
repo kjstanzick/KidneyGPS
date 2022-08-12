c176fc3a7c5a3d7ff242ca0d7035f50e22a6b28c
kjstanzick <110981634+kjstanzick@users.noreply.github.com>
2022-08-12 14:04:43 +0200

    Create create_folders.r

---
+++ create_folders.r
@@ -0,0 +1,14 @@
+####### create folders for all following steps
+
+## change directory to 
+
+setwd("/stk05236/clean_GPS/")
+
+dir.create("./inputs")
+dir.create("./01_EUR")
+dir.create("./02_cys_bun")
+dir.create("./03_candidate_genes")
+dir.create("./04_locus_table")
+dir.create("./05_cred_var")
+dir.create("./06_signal_table")
+
